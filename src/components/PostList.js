5b19e034949a003306687e2c43f67caa032cecc2
Tony Pham <tony.p@lashowroom.com>
2020-12-21 12:21:29 -0800

    more set-up

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class PostList extends React.Component {
+    render() {
+        return (
+            <div>post list</div>
+        );
+    };
+}
+
+export default PostList;
