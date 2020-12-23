d04a5c45db795e4790268572f424514fd01ad39f
Tony Pham <tony.p@lashowroom.com>
2020-12-22 22:22:20 -0800

    integrated lodash and used memoize so that same network requests aren't make duplicate times

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceHolder from "../api/jsonPlaceHolder";
 
 export const fetchPosts = () => {
@@ -9,9 +10,12 @@ export const fetchPosts = () => {
 };
 
 export const fetchUser = (userId) => {
-    return async dispatch => {
-        const response = await jsonPlaceHolder.get(`/users/${userId}`);
-
-        dispatch({type: 'FETCH_USER', payload: response.data})
+    return dispatch => {
+        _fetchUser(userId, dispatch);
     };
 };
+
+const _fetchUser = _.memoize(async (userId, dispatch) => {
+    const response = await jsonPlaceHolder.get(`/users/${userId}`);
+    dispatch({type: 'FETCH_USER', payload: response.data})
+});
